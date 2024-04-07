42dfdf6583507e804f4d8e19131eb7e249091b88
hankliu <397694072@qq.com>
2024-04-07 09:40:22 +0800

    fix(common): refine public path

--- src/index.ts
+++ src/index.ts
@@ -49,11 +49,13 @@ export class AutoBaseWebpackPlugin {
           if (this.prefix) {
             // 获取webpack配置中的output对象
             const outputOptions = compiler.options.output;
+            // output对象中的publicPath属性
+            const publicPath = outputOptions.publicPath;
 
             // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
             // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
             // 修改publicPath
-            outputOptions.publicPath = `/${this.prefix}/${outputOptions.publicPath}/`.replace(/\/\//g, '/');
+            outputOptions.publicPath = typeof publicPath === 'string' && publicPath.startsWith(`/${this.prefix}`) ? outputOptions.publicPath : `/${this.prefix}/${outputOptions.publicPath}/`.replace(/\/\//g, '/');
           }
         }
       }
